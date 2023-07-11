0076a5f06d5d645d5d3d147cf17508318e7dad5a
sofia-iliana <sofia.iliana1311@gmail.com>
2023-07-12 00:12:14 +0300

    main functionality done

--- backend/controllers/replyControllers.js
+++ backend/controllers/replyControllers.js
@@ -14,13 +14,6 @@ const getReplyByUserId = async (req, res) => {
   res.send(replyMsg);
 };
 
-/*async function getReplyByUserId  (req,res){
-  const replyMsg = await Reply.find({
-    userId: req.params.userId,
-  });
-  res.send(replyMsg);
-}*/
-
 //for the user to delete a reply, not sure if we are using this one
 const deleteReply = async (req, res) => {
   await Reply.deleteOne({ _id: req.params.id });
@@ -28,7 +21,3 @@ const deleteReply = async (req, res) => {
 };
 
 module.exports = { sendReply, getReplyByUserId, deleteReply };
-
-//{
-//  userId: req.params.userId,
-//}
